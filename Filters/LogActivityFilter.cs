14d05dd5cf60462840e4a655ce285106f53b4f66
Eyas Wannous <eyaswannous@gmail.com>
2024-03-29 10:35:48 +0300

    Update 'LogActivityFilter'

--- Filters/LogActivityFilter.cs
+++ Filters/LogActivityFilter.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace api.Filters;
 
-// we should extend on of those IActionFilter, IAsyncActionFilter 
+// we should extend one of those IActionFilter, IAsyncActionFilter 
 public class LogActivityFilter(ILogger<LogActivityFilter> logger) : IActionFilter, IAsyncActionFilter
 {
     private readonly ILogger<LogActivityFilter> _logger = logger;
@@ -12,25 +12,45 @@ public class LogActivityFilter(ILogger<LogActivityFilter> logger) : IActionFilte
     public void OnActionExecuting(ActionExecutingContext context)
     {
         // context.Result = new NotFoundResult(); // short circuit
-        _logger.LogInformation($"Executing action: {context.ActionDescriptor.DisplayName}"
-            + $"on Controller: {context.Controller}"
-            + $"with Arguments: {JsonSerializer.Serialize(context.ActionArguments)}");
+        _logger.LogInformation(
+            "Executing action: #{context.ActionDescriptor.DisplayName}"
+            + "on Controller: #{context.Controller}"
+            + "with Arguments: #{JsonSerializer.Serialize(context.ActionArguments)}",
+            context.ActionDescriptor.DisplayName,
+            context.Controller,
+            JsonSerializer.Serialize(context.ActionArguments)
+        );
     }
 
 
     public void OnActionExecuted(ActionExecutedContext context)
     {
-        _logger.LogInformation($"Action: {context.ActionDescriptor.DisplayName}"
-            + $"Executed on Controller: {context.Controller}");
+        _logger.LogInformation(
+            "Action: #{context.ActionDescriptor.DisplayName}"
+            + "Executed on Controller: #{context.Controller}",
+            context.ActionDescriptor.DisplayName,
+            context.Controller
+        );
     }
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        _logger.LogInformation($"(Async) Executing action: {context.ActionDescriptor.DisplayName}"
-            + $"on Controller: {context.Controller}"
-            + $"with Arguments: {JsonSerializer.Serialize(context.ActionArguments)}");
+        _logger.LogInformation(
+            "(Async) Executing action: #{context.ActionDescriptor.DisplayName}"
+            + "on Controller: #{context.Controller}"
+            + "with Arguments: #{JsonSerializer.Serialize(context.ActionArguments)}",
+            context.ActionDescriptor.DisplayName,
+            context.Controller,
+            JsonSerializer.Serialize(context.ActionArguments)
+        );
+
         await next();
-        _logger.LogInformation($"(Async) Action: {context.ActionDescriptor.DisplayName}"
-            + $"Executed on Controller: {context.Controller}");
+
+        _logger.LogInformation(
+            "(Async) Action: #{context.ActionDescriptor.DisplayName}"
+            + "Executed on Controller: #{context.Controller}",
+            context.ActionDescriptor.DisplayName,
+            context.Controller
+        );
     }
 }
